0a71e6efb9e4c7f9ea745028fb9deba56fa71079
webuser <ryan@virtuecenter.com>
2013-10-18 14:01:50 -0400

    security fix

--- src/PubSub/PubSubBuild.php
+++ src/PubSub/PubSubBuild.php
@@ -5,7 +5,7 @@ class PubSubBuild {
 	public function build ($root) {
 		$cache = [];
 		$listersCache = $root . '/../subscribers/_build.php';
-		$files = glob($root . '/../../subscribers/*.php');
+		$files = glob($root . '/../subscribers/*.php');
 		if (count($files) == 0) {
 			file_put_contents($listersCache, '<?php' . "\n" . 'return [];');
 			return;
