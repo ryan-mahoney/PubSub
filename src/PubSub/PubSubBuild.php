e903737ab6ee14ce686a62d7ca0f6fa53790e285
webuser <ryan@virtuecenter.com>
2013-10-24 23:12:52 -0400

    better topic file validation

--- src/PubSub/PubSubBuild.php
+++ src/PubSub/PubSubBuild.php
@@ -21,6 +21,13 @@ class PubSubBuild {
 		$cache = substr($cache, 0, -3);
 		$cache .= "\n" . '];';
 		file_put_contents($listersCache, $cache);
-		echo exec('php -l ' . $listersCache), "\n";
+		ob_start();
+		echo exec('php -l ' . $listersCache);
+		$buffer = ob_get_clean();
+		if (substr_count($buffer, 'No syntax errors detected in') == 1) {
+			echo 'Good: Topic build file is passing PHP error check.', "\n\n";
+		} else {
+			echo 'Problem: Topic build file is not passing PHP error check.', "\n\n";
+		}
 	}
 }
