7e7a8b7242d2d29631bda3b1c1e6e1f4e5d5b00b
webuser <ryan@virtuecenter.com>
2013-12-06 21:39:18 -0500

    tweets are being fetched

--- available/TweetsFetch.php
+++ available/TweetsFetch.php
@@ -1,4 +1,7 @@
 <?php
 return function ($context, $twitter) {
-	$twitter->externalFetch($context['value'], $context['expire'], $context['type']);
+	$tweets = $twitter->externalFetch($context['value'], $context['expire'], $context['type'], true);
+	if (is_array($tweets) && count($tweets) > 0) {
+		$twitter->save($context['type'], $context['value'], $tweets);
+	}
 };
