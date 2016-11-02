ac85670529f12f85c87ddabd9f23203cfdb15dd1
Ryan Mahoney <ryan.mahoney@betterlesson.com>
2016-11-02 12:59:19 -0400

    remove hard-coded verions

--- available/CollectionStats.php
+++
@@ -1,4 +0,0 @@
-<?php
-return function ($context, $collection) {
-    $collection->statsSet($context['dbURI']);
-};
--- available/TweetsFetch.php
+++
@@ -1,7 +0,0 @@
-<?php
-return function ($context, $twitter) {
-    $tweets = $twitter->externalFetch($context['value'], $context['expire'], $context['type'], true);
-    if (is_array($tweets) && count($tweets) > 0) {
-        $twitter->save($context['type'], $context['value'], $tweets);
-    }
-};
